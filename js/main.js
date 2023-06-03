b2cbacaf852824e39fbc4d106c4453e77294a5d3
altairson <enivoid@gmail.com>
2023-06-03 15:51:29 +0400

    added step 2 progress

--- js/main.js
+++ js/main.js
@@ -127,36 +127,7 @@ $(document).ready(function() {
     }
 
     write_in_google_sheets();
-
-    function saveUserInfo(user) {
-        console.log(user);
-    }
-
-
-    function showProgress(percent = 10) {
-        $(".num")[0].innerText = percent;
-        let item = $(".block")[0];
-        let numElement = item.querySelector('.num');
-        let num = parseInt(numElement.innerText);
-        let count = 0;
-        let time = 2000 / num;
-        let circle = item.querySelector('.circle');
-        setInterval(() => {
-            if(count == num){
-                clearInterval();
-            } 
-            else {
-                count += 1;
-                numElement.innerText = count;
-            }
-        }, time)
-        circle.style.strokeDashoffset = 503 - ( 503 * ( num / 100 ));
-        let dots = item.querySelector('.dots');
-        dots.style.transform = `rotate(${360 * (num / 100)}deg)`;
-        if(num == 100){
-            dots.style.opacity = 0;
-        }
-    }
-    showProgress();
     
+    showProgress();
+
 });
